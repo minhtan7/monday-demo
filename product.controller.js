c27ff7e49df8d3e5e5a7a0bffbf393ab82d46296
Your Name <you@example.com>
2021-08-02 17:14:38 +0700

    first push

---
+++ product.controller.js
@@ -0,0 +1,6 @@
+const produuctController = {};
+produuctController.getAllProducts = async (req, res, next) => {
+  console.log("haha");
+};
+
+module.exports = produuctController;
---
+++ user.controller.js
@@ -0,0 +1,6 @@
+const userController = {};
+userController.getCurrentUser = async (req, res, next) => {
+  console.log("haha");
+};
+
+module.exports = userController;
